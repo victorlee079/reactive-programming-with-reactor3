5e1e601c369655b25d896f9136de0a33e1ecb2a9
Victor Lee <victorlee079@gmail.com>
2022-10-10 15:39:43 +0800

    Update Part11BlockingToReactive.java

--- Part11BlockingToReactive.java
+++ Part11BlockingToReactive.java
@@ -41,7 +41,7 @@ public class Part11BlockingToReactive {
 
 	// TODO Insert users contained in the Flux parameter in the blocking repository using a bounded elastic scheduler and return a Mono<Void> that signal the end of the operation
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
-		return flux.publishOn(Schedulers.elastic()).doOnNext(repository::save).then();
+		return flux.publishOn(Schedulers.boundedElastic()).doOnNext(repository::save).then();
 	}
 
 }
